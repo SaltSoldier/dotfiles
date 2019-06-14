cd067bec1dc54c8bf6d621a041593195f4b0ad1f
Ryohei Shioji <rshioji@yahoo-corp.jp>
2019-06-14 14:51:32 +0900

    設定ファイル導入shellファイル追記

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,4 +1,22 @@
-ln -sh ~/dotfiles/.vimrc ~/.vimrc
-ln -sh ~/dotfiles/.bashrc ~/.bashrc
-ln -sh ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sh ~/dotfiles/.vimrc-neocomplete ~/.vim/.vimrc-neocomplete
+# 初めからあるファイルを削除
+rm ~/.vimrc
+rm ~/.tmux.conf
+rm -rf ~/.vim
+rm -rf ~/.config/nvim
+
+# ディレクトリ生成
+mkdir ~/.vim
+mkdir ~/.vim/dein
+mkdir ~/.config
+mkdir ~/.config/nvim
+
+# .dotfiles内のエイリアス貼り付け
+ln -sh ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -sh ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sh ~/.dotfiles/vim/.vimrc-neocomplete ~/.vim/.vimrc-neocomplete
+ln -sh ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sh ~/.dotfiles/git/.gitconfig ~/.gitconfig
+ln -sh ~/.dotfiles/dein/dein.toml ~/.vim/dein/dein.toml
+
+# deinのインストール
+sh ~/.dotfiles/dein/installer.sh ~/.vim/dein
